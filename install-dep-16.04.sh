6c2d29c88bb25328b7d4372af0e00e519657980d
Jon <jgoenetxea@gmail.com>
2017-07-30 15:10:01 +0100

    Updated last step

--- install-dep-16.04.sh
+++ install-dep-16.04.sh
@@ -155,11 +155,15 @@ echo
 echo 'Almost done!'
 echo
 
-git clone https://github.com/torch/nn && ( cd nn && git checkout getParamsByDevice && luarocks make rocks/nn-scm-1.rockspec )
+cd $dir
+git clone https://github.com/torch/nn 
+cd nn
+luarocks make rocks/nn-scm-1.rockspec 
 
-git clone https://github.com/facebook/fbtorch.git && ( cd fbtorch && luarocks make rocks/fbtorch-scm-1.rockspec )
-
-git clone https://github.com/facebook/fbnn.git && ( cd fbnn && luarocks make rocks/fbnn-scm-1.rockspec )
+cd $dir
+git clone https://github.com/jgoenetxea/fbtorch.git
+cd fbtorch 
+luarocks make rocks/fbtorch-scm-1.rockspec
 
 
 echo
